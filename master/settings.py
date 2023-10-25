1dce3e3100146b1b1f4c1e4dd71711a3477213df
Schleidens <alectineschleidens2016@gmail.com>
2023-10-25 16:12:43 -0400

    create homepage and set routes for static files

--- master/settings.py
+++ master/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # project apps
     'authentication',
+    'userProfile',
 
     # third party apps
     'bootstrap5'
@@ -127,6 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# root for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.joinpath('media/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ userProfile/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.conf import settings
+from django.core.exceptions import ValidationError
+
+# Create your models here.
+
+
+# validator for bio length
+def validate_bio_length(value):
+    if len(value) > 2000:
+        raise ValidationError('Content is too long, 1000 character max')
+
+
+class teacherProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name=(
+        "Teacher"), related_name="teacher", on_delete=models.CASCADE)
+    bio = models.TextField(max_length=1000, validators=[validate_bio_length])
+    qualifications = models.CharField(max_length=50)
+    profile_picture = models.ImageField(
+        upload_to="profile/", blank=True)
+
+    def __str__(self):
+        return self.user.username
