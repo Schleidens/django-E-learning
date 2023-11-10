b472ccb13aef3dec420a29c89fba5380da5ed333
Schleidens <alectineschleidens2016@gmail.com>
2023-11-09 23:12:40 -0500

    add profile view dynacally for student or teacher on the same route

--- userProfile/urls.py
+++ userProfile/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import homeView
+from .views import homeView, userProfileView
 
 urlpatterns = [
     path('', homeView, name='home-page'),
+    path('profile', userProfileView.as_view(), name='user-profile-page'),
 ]
--- userProfile/views.py
+++ userProfile/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
+
+from .models import studentProfile, teacherProfile
 
 # Create your views here.
 
 
 def homeView(request):
     return render(request, 'home.html')
+
+
+class userProfileView(LoginRequiredMixin, View):
+    student_profile_model = studentProfile
+    teacher_profile_model = teacherProfile
+    template = ''
+
+    def get(self, request):
+        if request.user.is_teacher:
+            model = self.teacher_profile_model
+            template = 'teacher_profile_template.html'
+        else:
+            model = self.student_profile_model
+            template = 'student_profile_template.html'
+
+        profile = get_object_or_404(model, user=request.user)
+
+        return render(request, template, {'profile': profile})
+
+    def post(self, request):
+        pass
