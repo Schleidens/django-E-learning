5244e23b7c943f463e22738f9750729f1a9b9094
Schleidens <alectineschleidens2016@gmail.com>
2024-01-03 22:50:57 -0500

    create course model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from django.conf import settings
+from django.utils.text import slugify
 
 # Create your models here.
+
+
+class coursesModel(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=(
+        "Author"), on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, editable=False)
+    cover = models.ImageField(upload_to='courses')
+    description = RichTextField()
+    duration = models.PositiveIntegerField()
+    intro_video = models.URLField(max_length=200)
+    created_at = models.DateField(auto_now_add=True)
+    last_update = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
